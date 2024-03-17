c78a8296a0eea9b936ac5222bf54c706c75a9746
Volk <a4586658@gmail.com>
2024-03-17 21:54:11 +0800

    Add initial pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
+import ShortTerm from "./pages/ShortTerm";
+import LongTerm from "./pages/LongTerm";
+import Setting from "./pages/Setting";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/shortTerm",
+        element: <ShortTerm />,
+      },
+      {
+        path: "/longTerm",
+        element: <LongTerm />,
+      },
+      {
+        path: "/setting",
+        element: <Setting />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div className="App">Empire</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/LongTerm.tsx
@@ -0,0 +1,3 @@
+export default function LongTerm() {
+  return <div>LongTerm</div>;
+}
