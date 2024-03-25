c8f4f5538304d4be1e9dbaea1566891085bea865
Volk <a4586658@gmail.com>
2024-03-25 18:09:17 +0800

    Add basic style to the nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,32 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div>
-      <Link to="/">Empire</Link>
-      <Link to="/shortTerm">Short Term</Link>
-      <Link to="/longTerm">Long Term</Link>
-      <Link to="/setting">Setting</Link>
+      <div className="flex justify-between px-4 border-b">
+        <Link
+          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/"
+        >
+          Empire
+        </Link>
+        <Link
+          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/shortTerm"
+        >
+          Short Term
+        </Link>
+        <Link
+          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/longTerm"
+        >
+          Long Term
+        </Link>
+        <Link
+          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          to="/setting"
+        >
+          Setting
+        </Link>
+      </div>
     </div>
   );
 }
