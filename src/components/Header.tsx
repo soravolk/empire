f41cb848117bb2e004bf19ecf99e6d9acba5bd65
Volk <a4586658@gmail.com>
2024-03-25 18:39:20 +0800

    Add a temporary app logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import { GiWhiteTower } from "react-icons/gi";
 
 export default function Header() {
   return (
     <div>
       <div className="flex justify-between px-4 border-b">
         <Link
-          className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+          className="text-gray-300 hover:text-black rounded-md px-3 py-2 text-sm font-medium"
           to="/"
         >
-          Empire
+          <GiWhiteTower className="h-5" />
         </Link>
         <Link
           className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
