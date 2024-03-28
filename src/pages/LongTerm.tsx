c184d3f733874663e198debf46b11dfee4e88263
Volk <a4586658@gmail.com>
2024-03-28 16:04:34 +0800

    Show subcategories based on selected cycle and category with mocked data

--- src/pages/LongTerm.tsx
+++ src/pages/LongTerm.tsx
@@ -23,18 +23,47 @@ const CATEGORIES = [
     name: "Machine Learning",
   },
   {
-    id: 0,
+    id: 1,
     cycle_id: 0,
     category_id: 1,
     name: "System Design",
   },
   {
-    id: 0,
+    id: 2,
     cycle_id: 0,
     category_id: 2,
     name: "Fullstack",
   },
 ];
+
+const SUBCATEGORIES = [
+  {
+    id: 0,
+    cycle_id: 0,
+    subcategory_id: 0,
+    category_id: 2,
+    name: "Next.js",
+  },
+  {
+    id: 1,
+    cycle_id: 0,
+    subcategory_id: 1,
+    category_id: 2,
+    name: "NestJS",
+  },
+];
+
+interface SubcategoryProps {
+  cycle: number | null;
+  category: number | null;
+  subcategories: {
+    id: number;
+    cycle_id: number;
+    category_id: number;
+    subcategory_id: number;
+    name: string;
+  }[];
+}
 
 interface CategoryProps {
   cycle: number | null;
@@ -44,6 +73,7 @@ interface CategoryProps {
     category_id: number;
     name: string;
   }[];
+  setCategory: (category: number | null) => void;
 }
 
 interface CycleProps {
@@ -75,8 +105,43 @@ const Cycle: React.FC<CycleProps> = ({ cycles, setCycle }) => {
   );
 };
 
-const Category: React.FC<CategoryProps> = ({ cycle, categories }) => {
+const Category: React.FC<CategoryProps> = ({
+  cycle,
+  categories,
+  setCategory,
+}) => {
   const displayItems = categories.filter((item) => item.cycle_id === cycle);
+  const handleClick = (id: number) => {
+    setCategory(id);
+  };
+
+  return (
+    <div className="w-1.5/5">
+      <div className="flex flex-col items-center space-y-4 mx-5 p-4">
+        {displayItems.map((item) => (
+          <button
+            className="items-center justify-center bg-gray-300 w-20 h-15 p-2 rounded"
+            onClick={() => handleClick(item.category_id)}
+          >
+            {item.name}
+          </button>
+        ))}
+        <button className="items-center justify-center bg-blue-500 text-white rounded-full h-12 w-12">
+          +
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const SubCategory: React.FC<SubcategoryProps> = ({
+  cycle,
+  category,
+  subcategories,
+}) => {
+  const displayItems = subcategories.filter(
+    (item) => item.cycle_id === cycle && item.category_id === category
+  );
 
   return (
     <div className="w-1.5/5">
@@ -93,22 +158,6 @@ const Category: React.FC<CategoryProps> = ({ cycle, categories }) => {
     </div>
   );
 };
-
-const SubCategory = () => (
-  <div className="w-1.5/5">
-    <div className="flex flex-col items-center space-y-4 mx-5 p-4">
-      <button className="items-center justify-center bg-gray-300 w-20 h-15 p-2 rounded">
-        Next.js
-      </button>
-      <button className="items-center justify-center bg-gray-300 w-20 h-15 p-2 rounded">
-        NestJS
-      </button>
-      <button className="items-center justify-center bg-blue-500 text-white rounded-full h-12 w-12">
-        +
-      </button>
-    </div>
-  </div>
-);
 
 const Content = () => (
   <div>
@@ -128,12 +177,21 @@ const Content = () => (
 
 export default function LongTerm() {
   const [cycle, setCycle] = useState<number | null>(null);
+  const [category, setCategory] = useState<number | null>(null);
 
   return (
     <div className="flex">
       <Cycle cycles={CYCLES} setCycle={setCycle} />
-      <Category cycle={cycle} categories={CATEGORIES} />
-      <SubCategory />
+      <Category
+        cycle={cycle}
+        categories={CATEGORIES}
+        setCategory={setCategory}
+      />
+      <SubCategory
+        cycle={cycle}
+        category={category}
+        subcategories={SUBCATEGORIES}
+      />
       <Content />
     </div>
   );
