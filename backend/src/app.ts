d8c5682f97e02d3bcd52e36f76a225bd3758c164
Volk <a4586658@gmail.com>
2024-04-06 22:02:30 +0800

    Add basic create, list, and delete APIs for content

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import UserRoutes from "./routes/user";
 import longTermRoutes from "./routes/longTerm";
 import categoryRoutes from "./routes/category";
 import subcategoryRoutes from "./routes/subcategory";
+import contentRoutes from "./routes/content";
 
 const app: Express = express();
 
@@ -11,6 +12,7 @@ app.use("/users", UserRoutes);
 app.use("/longTerms", longTermRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/subcategories", subcategoryRoutes);
+app.use("/contents", contentRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
---
+++ backend/src/controllers/content.ts
@@ -0,0 +1,33 @@
+import { RequestHandler } from "express";
+import db from "../db/utils";
+
+const createContent: RequestHandler = async (req, res) => {
+  const { subcategory_id, name } = req.body;
+  try {
+    await db.insert("contents", { subcategory_id, name });
+    res.status(201).send();
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+
+const getContents: RequestHandler = async (req, res) => {
+  try {
+    const { rows } = await db.getAll("contents");
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({ error: "internal server error" });
+  }
+};
+
+const deleteContent: RequestHandler = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await db.remove("contents", { id });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: "internal server error" });
+  }
+};
+
+export default { createContent, getContents, deleteContent };
