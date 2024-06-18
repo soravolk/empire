a8bfe531885afd7596e879db7a6bd24c954437b6
Volk <a4586658@gmail.com>
2024-06-18 22:50:48 +0200

    Fix missing express-session type

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,7 +24,7 @@ app.use(
 
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET!,
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }, // TODO: https
