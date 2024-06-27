2d1fe7a3bc03d684e7edf5fb9b96abab00f156e9
Volk <a4586658@gmail.com>
2024-06-27 21:39:27 +0200

    Add key to each category map item and types

--- web/src/components/Category.tsx
+++ web/src/components/Category.tsx
@@ -78,13 +78,15 @@ const Category: React.FC<CategoryProps> = ({
   return (
     <div className="w-1.5/5">
       <div className="flex flex-col items-center space-y-4 mx-5 p-4">
-        {categories.map((item) => (
-          <button
-            className="items-center justify-center bg-gray-300 w-20 h-15 p-2 rounded"
-            onClick={() => handleClick(item)}
-          >
-            {item.name}
-          </button>
+        {categories.map((item: CategoryItem, id: number) => (
+          <div key={id}>
+            <button
+              className="items-center justify-center bg-gray-300 w-20 h-15 p-2 rounded"
+              onClick={() => handleClick(item)}
+            >
+              {item.name}
+            </button>
+          </div>
         ))}
         <button
           className="items-center justify-center bg-blue-500 text-white rounded-full h-12 w-12"
