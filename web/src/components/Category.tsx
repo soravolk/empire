e0ea8d2bcb04e903eac58442545e199c65d20344
Volk <a4586658@gmail.com>
2024-07-01 22:54:32 +0200

    Export category form as a component

--- web/src/components/Category.tsx
+++ web/src/components/Category.tsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
 import { CycleCategoryItem, CycleItem, User } from "../types";
-import { CiCircleCheck } from "react-icons/ci";
-import {
-  useAddCategoryMutation,
-  useAddCategoryToCycleMutation,
-  useDeleteCategoryFromCycleMutation,
-} from "../store";
+import { useDeleteCategoryFromCycleMutation } from "../store";
 import { MdDelete } from "react-icons/md";
+import CategoryForm from "./CategoryForm";
 
 interface CategoryProps {
   categories: CycleCategoryItem[];
@@ -14,56 +10,6 @@ interface CategoryProps {
   user: User;
   cycle: CycleItem | null;
 }
-
-interface FormControlProps {
-  setExpandForm: (expandForm: boolean) => void;
-  user: User;
-  cycle: CycleItem | null;
-}
-
-const CategoryForm: React.FC<FormControlProps> = ({
-  setExpandForm,
-  user,
-  cycle,
-}) => {
-  const [newCategoryName, setNewCategoryName] = useState("");
-  const [addCategory, addCategoryResults] = useAddCategoryMutation();
-  const [addCategoryToCycle, addCategoryToCycleResults] =
-    useAddCategoryToCycleMutation();
-
-  const handleAddCategory = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const result = await addCategory({
-      userId: user.id,
-      name: newCategoryName,
-    });
-    // TODO: add error handling
-    if ("data" in result && cycle != null) {
-      addCategoryToCycle({ cycleId: cycle.id, categoryId: result.data.id });
-    }
-    setExpandForm(false);
-  };
-  return (
-    <form
-      className="flex flex-col items-center border p-2 space-y-2"
-      onSubmit={handleAddCategory}
-    >
-      <label>
-        Name:
-        <input
-          className="ml-2 border"
-          type="text"
-          name="name"
-          value={newCategoryName}
-          onChange={(e) => setNewCategoryName(e.target.value)}
-        />
-      </label>
-      <button type="submit">
-        <CiCircleCheck />
-      </button>
-    </form>
-  );
-};
 
 const Category: React.FC<CategoryProps> = ({
   categories,
