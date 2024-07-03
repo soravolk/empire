cb59b14cab96460ee6119b2076fb2e2bba69e079
Volk <a4586658@gmail.com>
2024-07-03 22:29:28 +0200

    Add tag for cycle content to refetch upon creating

--- web/src/store/apis/cyclesApi.tsx
+++ web/src/store/apis/cyclesApi.tsx
@@ -3,6 +3,7 @@ import {
   CycleCategoryItem,
   CycleSubcategoryItem,
   CycleItem,
+  ContentItem,
 } from "../../types";
 
 const cyclesApi = createApi({
@@ -10,7 +11,7 @@ const cyclesApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:5000/cycles",
   }),
-  tagTypes: ["Cycle", "LongTerm", "Category", "Subcategory"],
+  tagTypes: ["Cycle", "LongTerm", "Category", "Subcategory", "Content"],
   endpoints(builder) {
     return {
       fetchCyclesOfLongTerm: builder.query({
@@ -62,6 +63,13 @@ const cyclesApi = createApi({
         },
       }),
       fetchContentsFromCycle: builder.query({
+        providesTags: (result, error, cycle) => {
+          const tags = result.map((content: ContentItem) => {
+            return { type: "Content", id: content.id };
+          });
+          tags.push({ type: "Cycle", id: cycle.id });
+          return tags;
+        },
         query: (cycle) => {
           return {
             url: `/${cycle.id}/contents`,
@@ -147,6 +155,9 @@ const cyclesApi = createApi({
         },
       }),
       addContentToCycle: builder.mutation({
+        invalidatesTags: (result, error, arg) => {
+          return [{ type: "Cycle", id: arg.cycleId }];
+        },
         query: ({ cycleId, contentId }) => {
           return {
             method: "POST",
