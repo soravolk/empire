a73e77df411ae516e60c284148e7d6d0f4c7c5cc
Volk <a4586658@gmail.com>
2024-07-23 22:03:54 +0200

    Refactor to avoid calling RTK query with null state

--- web/src/pages/LongTerm.tsx
+++ web/src/pages/LongTerm.tsx
@@ -22,22 +22,66 @@ import Content from "../components/Content";
 import CycleContext from "../context/cycle";
 
 export default function LongTerm() {
-  const [cycle, setCycle] = useState<CycleItem | null>(null);
   const [longTerm, setLongTerm] = useState<LongTermItem | null>(null);
-  const [category, setCategory] = useState<CycleCategoryItem | null>(null);
-  const [subcategory, setSubcategory] = useState<CycleSubcategoryItem | null>(
-    null
-  );
   const {
     data: longTermData,
     error: longTermError,
     isLoading: isLongTermLoading,
   } = useFetchLongTermsQuery(null);
+
+  return (
+    <div className="flex flex-col">
+      <div className="w-full mb-4">
+        {longTermData && (
+          <Dropdown
+            options={getLongTermHistoryOptions(longTermData)}
+            onSelect={setLongTerm}
+          />
+        )}
+      </div>
+      <div className="flex px-5 py-2">
+        {longTerm && <CycleOptions longTerm={longTerm} />}
+      </div>
+    </div>
+  );
+}
+
+interface ItemProps {
+  cycle: CycleItem;
+}
+
+interface CycleOptionsProps {
+  longTerm: LongTermItem;
+}
+
+const CycleOptions: React.FC<CycleOptionsProps> = ({ longTerm }) => {
   const {
     data: cycleData,
     error: cycleFetchError,
     isLoading: isCycleLoading,
   } = useFetchCyclesOfLongTermQuery(longTerm);
+  const [cycle, setCycle] = useState<CycleItem | null>(null);
+
+  return (
+    <CycleContext.Provider value={cycle}>
+      <div className="basis-1/4">
+        {cycleData && <Cycle cycles={cycleData} setCycle={setCycle} />}
+      </div>
+      {cycle && <Items cycle={cycle} />}
+    </CycleContext.Provider>
+  );
+};
+
+const Items: React.FC<ItemProps> = ({ cycle }) => {
+  const [category, setCategory] = useState<CycleCategoryItem | null>(null);
+  const [subcategory, setSubcategory] = useState<CycleSubcategoryItem | null>(
+    null
+  );
+  useEffect(() => {
+    setCategory(null);
+    setSubcategory(null);
+  }, [cycle]);
+
   const {
     data: categoryData,
     error: categoryFetchError,
@@ -58,52 +102,31 @@ export default function LongTerm() {
     error: userFetchError,
     isLoading: isUserLoading,
   } = useFetchCurrentUserQuery(null);
-
-  useEffect(() => {
-    setCategory(null);
-    setSubcategory(null);
-  }, [cycle]);
-
   return (
-    <CycleContext.Provider value={cycle}>
-      <div className="flex flex-col">
-        <div className="w-full mb-4">
-          {longTermData && (
-            <Dropdown
-              options={getLongTermHistoryOptions(longTermData)}
-              onSelect={setLongTerm}
-            />
-          )}
-        </div>
-        <div className="flex px-5 py-2">
-          <div className="basis-1/4">
-            {cycleData && <Cycle cycles={cycleData} setCycle={setCycle} />}
-          </div>
-          <div className="basis-1/4">
-            {cycle && categoryData && (
-              <Category
-                categories={categoryData}
-                setCategory={setCategory}
-                user={userData}
-              />
-            )}
-          </div>
-          <div className="basis-1/4">
-            {cycle && category && subcategoryData && (
-              <SubCategory
-                category={category}
-                subcategories={subcategoryData}
-                setSubcategory={setSubcategory}
-              />
-            )}
-          </div>
-          <div className="basis-1/4">
-            {cycle && subcategory && contentData && (
-              <Content subcategory={subcategory} contents={contentData} />
-            )}
-          </div>
-        </div>
+    <>
+      <div className="basis-1/4">
+        {categoryData && (
+          <Category
+            categories={categoryData}
+            setCategory={setCategory}
+            user={userData}
+          />
+        )}
       </div>
-    </CycleContext.Provider>
+      <div className="basis-1/4">
+        {category && subcategoryData && (
+          <SubCategory
+            category={category}
+            subcategories={subcategoryData}
+            setSubcategory={setSubcategory}
+          />
+        )}
+      </div>
+      <div className="basis-1/4">
+        {subcategory && contentData && (
+          <Content subcategory={subcategory} contents={contentData} />
+        )}
+      </div>
+    </>
   );
-}
+};
