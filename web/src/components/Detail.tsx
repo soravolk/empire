588459aa06de9bb1ec6cbc62518a54df7d4b089f
soravolk <volk.huang@gmail.com>
2024-10-20 19:01:07 +0200

    Show a form for creating detail items

--- web/src/components/Detail.tsx
+++ web/src/components/Detail.tsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { CycleContentItem } from "../types";
+import ItemCreationButton from "./ItemCreationButton";
+import CreationForm from "./CreationForm";
 
 type DetailProps = {
   content: CycleContentItem;
 };
 
+type FormControlProps = {
+  setExpandForm: (expand: boolean) => void;
+};
+
+const DetailForm: React.FC<FormControlProps> = ({ setExpandForm }) => {
+  const handleAddDetail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setExpandForm(false);
+  };
+
+  return <CreationForm handleAddFunc={handleAddDetail} />;
+};
 const Detail: React.FC<DetailProps> = ({ content }) => {
+  const [expandForm, setExpandForm] = useState<boolean>(false);
+
+  const handleAddDetail = () => {
+    setExpandForm(!expandForm);
+  };
+
   return (
-    <div className="p-4 bg-white shadow rounded-lg">
+    <div className="flex flex-col p-4 bg-white shadow rounded-lg">
       Content: {content.name}
+      <ItemCreationButton handleClick={handleAddDetail} />
+      {expandForm && <DetailForm setExpandForm={setExpandForm} />}
     </div>
   );
 };
