6eba75fc5c3aa71b2008756ba4c85c2ed7cad1ca
soravolk <volk.huang@gmail.com>
2024-11-02 23:47:20 +0100

    Add a button, icon, and corresponding stylings to create short term

--- web/src/pages/ShortTerm.tsx
+++ web/src/pages/ShortTerm.tsx
@@ -4,6 +4,15 @@ import { CycleItem } from "../types";
 import { useCycleListContext } from "../context/cycle";
 import { getAvailableCycleOptions } from "../utils/utils";
 import { Items } from "./LongTerm";
+import { BsPencilSquare } from "react-icons/bs";
+
+const CreateShortTerm = () => {
+  return (
+    <button>
+      <BsPencilSquare />
+    </button>
+  );
+};
 
 export default function ShortTerm() {
   const [cycle, setCycle] = useState<CycleItem | null>(null);
@@ -11,13 +20,18 @@ export default function ShortTerm() {
 
   return (
     <div className="flex flex-col">
-      <div className="w-full mb-4">
-        {cycleList && (
-          <Dropdown
-            options={getAvailableCycleOptions(cycleList)}
-            onSelect={setCycle}
-          />
-        )}
+      <div className="flex items-center justify-between mb-4">
+        <div>
+          {cycleList && (
+            <Dropdown
+              options={getAvailableCycleOptions(cycleList)}
+              onSelect={setCycle}
+            />
+          )}
+        </div>
+        <div>
+          <CreateShortTerm />
+        </div>
       </div>
       <div className="flex px-5 py-2">
         {cycle && <Items cycle={cycle} shortTerm />}
