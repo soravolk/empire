798728c5a139ea54075e087b6bb0e9820f4ffdfc
soravolk <volk.huang@gmail.com>
2025-07-12 21:22:37 +0200

    [EPR-121] Add cycle copy workaround in long term page

--- web/src/components/Cycle.tsx
+++ web/src/components/Cycle.tsx
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react";
 import { CycleItem } from "../types";
-import { useAddCycleMutation, useDeleteCycleMutation } from "../store";
+import {
+  useAddCycleMutation,
+  useDeleteCycleMutation,
+  useAddCategoryToCycleMutation,
+  useAddSubcategoryToCycleMutation,
+  useAddContentToCycleMutation,
+  store,
+} from "../store";
+import { cyclesApi } from "../store/apis/cyclesApi";
 import Calendar from "react-calendar";
 import { MdDelete } from "react-icons/md";
+import { MdContentCopy } from "react-icons/md";
 import "react-calendar/dist/Calendar.css";
 import ItemCreationButton from "./ItemCreationButton";
 import { useLongTermContext } from "../context/longTerm";
@@ -26,6 +35,9 @@ const Cycle: React.FC<CycleProps> = ({
 
   const [addCycle, addCycleResults] = useAddCycleMutation();
   const [deleteCycle, deleteCycleResults] = useDeleteCycleMutation();
+  const [addCategoryToCycle] = useAddCategoryToCycleMutation();
+  const [addSubcategoryToCycle] = useAddSubcategoryToCycleMutation();
+  const [addContentToCycle] = useAddContentToCycleMutation();
   const [expandCalendar, setExpandCalendar] = useState(false);
 
   const handleClick = (cycle: CycleItem) => {
@@ -47,6 +59,77 @@ const Cycle: React.FC<CycleProps> = ({
     }
   };
 
+  const handleCopyCycle = async (cycle: CycleItem) => {
+    // Create a new cycle
+    const newCycleResult = await addCycle({
+      longTermId: longTerm?.id,
+      startTime: cycle.start_time,
+      endTime: cycle.end_time,
+    });
+    if (!("data" in newCycleResult) || !newCycleResult.data) {
+      throw new Error("Failed to create new cycle");
+    }
+    const newCycle = newCycleResult.data;
+
+    // Fetch all data from the original cycle using RTK Query
+    const categoriesPromise = store.dispatch(
+      cyclesApi.endpoints.fetchCategoriesFromCycle.initiate(cycle)
+    );
+    const subcategoriesPromise = store.dispatch(
+      cyclesApi.endpoints.fetchSubcategoriesFromCycle.initiate(cycle)
+    );
+    const contentsPromise = store.dispatch(
+      cyclesApi.endpoints.fetchContentsFromCycle.initiate(cycle)
+    );
+    const [categoriesResult, subcategoriesResult, contentsResult] =
+      await Promise.all([
+        categoriesPromise,
+        subcategoriesPromise,
+        contentsPromise,
+      ]);
+    const categories = categoriesResult.data;
+    const subcategories = subcategoriesResult.data;
+    const contents = contentsResult.data;
+    if (
+      !categoriesResult.data.length ||
+      !subcategoriesResult.data.length ||
+      !contentsResult.data.length
+    ) {
+      throw new Error("Failed to create new cycle");
+    }
+
+    // Recreate category/subcategory/contnet and their relationships
+    for (const category of categories) {
+      await addCategoryToCycle({
+        cycleId: newCycle.id,
+        categoryId: category.category_id,
+      });
+
+      const categorySubcategories = subcategories.filter(
+        (sub: any) => sub.category_id === category.category_id
+      );
+
+      for (const subcategory of categorySubcategories) {
+        await addSubcategoryToCycle({
+          cycleId: newCycle.id,
+          subcategoryId: subcategory.subcategory_id,
+        });
+
+        const subcategoryContents = contents.filter(
+          (content: any) =>
+            content.subcategory_id === subcategory.subcategory_id
+        );
+
+        for (const content of subcategoryContents) {
+          await addContentToCycle({
+            cycleId: newCycle.id,
+            contentId: content.content_id,
+          });
+        }
+      }
+    }
+  };
+
   return (
     <div className="flex flex-col items-center space-y-2">
       {cycles &&
@@ -59,6 +142,9 @@ const Cycle: React.FC<CycleProps> = ({
               onClick={() => handleClick(item)}
             >
               {`Cycle ${id + 1}`}
+            </button>
+            <button onClick={() => handleCopyCycle(item)}>
+              <MdContentCopy />
             </button>
             <button>
               <MdDelete onClick={() => deleteCycle(item)} />
