e90f79c0a7a7c1624326607d0b30d11a48ecfae8
soravolk <volk.huang@gmail.com>
2025-10-27 20:54:13 +0100

    [EPR-195] Wrap server with serverless-express tool

---
+++ backend/src/lambda.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+import serverlessExpress from "@codegenie/serverless-express";
+import { Handler } from "aws-lambda";
+import { createApp } from "./app";
+
+let serverlessExpressInstance: Handler;
+
+async function setup() {
+  const app = await createApp();
+  serverlessExpressInstance = serverlessExpress({ app });
+}
+
+export const handler: Handler = async (event, context, callback) => {
+  if (!serverlessExpressInstance) {
+    await setup();
+  }
+
+  return serverlessExpressInstance(event, context, callback);
+};
