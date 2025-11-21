8fe0b971231f00ad563583667f0e3d6f236fbcbc
soravolk <volk.huang@gmail.com>
2025-11-22 00:53:11 +0100

    [EPR-194] Prepare JWT based auth for lambda

---
+++ backend/src/authLambda.ts
@@ -0,0 +1,23 @@
+// Load dotenv only when not in AWS Lambda
+if (!process.env.AWS_LAMBDA_FUNCTION_NAME) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
+}
+
+import serverlessExpress from "@codegenie/serverless-express";
+import { Handler } from "aws-lambda";
+import { createAuthApp } from "./auth-app";
+
+let serverlessExpressInstance: Handler;
+
+async function setup() {
+  const app = await createAuthApp();
+  serverlessExpressInstance = serverlessExpress({ app });
+}
+
+export const handler: Handler = async (event, context, callback) => {
+  if (!serverlessExpressInstance) {
+    await setup();
+  }
+  return serverlessExpressInstance(event, context, callback);
+};
