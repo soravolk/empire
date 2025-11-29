08d17475592ee249cae94a370970afde3e4d5efb
soravolk <volk.huang@gmail.com>
2025-11-29 01:48:35 +0100

    ref EPR-169: Use AWS SSM for env variable to remove VPC endpoint cost

--- backend/src/db/postgre.ts
+++ backend/src/db/postgre.ts
@@ -1,30 +1,28 @@
 import { Pool } from "pg";
-import { getDatabaseSecret } from "./getSecret";
 
 export let pg: Pool | undefined = undefined;
 
 export const init = async () => {
-  // Local/dev: use environment variables
-  const { NODE_ENV, PG_USER, PG_HOST, PG_DATABASE } = process.env;
-  let user = PG_USER,
-    host = PG_HOST,
-    database = PG_DATABASE,
-    port = 5432,
-    password,
-    ssl;
-
-  // Use Secrets Manager in production
-  if (NODE_ENV === "production") {
-    ({ user, host, database, port, password, ssl } = await getDatabaseSecret());
-    ssl = { rejectUnauthorized: false }; // TODO: enable proper SSL validation with CA
+  const { NODE_ENV, PG_USER, PG_HOST, PG_DATABASE, PG_PASSWORD, PG_PORT } =
+    process.env;
+  if (
+    !PG_HOST ||
+    !PG_USER ||
+    (NODE_ENV === "production" && !PG_PASSWORD) ||
+    !PG_DATABASE
+  ) {
+    throw new Error(
+      "Missing required DB env vars (PG_HOST, PG_USER, PG_PASSWORD, PG_DATABASE)"
+    );
   }
 
   pg = new Pool({
-    user,
-    host,
-    database,
-    port,
-    password,
-    ssl,
+    user: PG_USER,
+    host: PG_HOST,
+    database: PG_DATABASE,
+    port: Number(PG_PORT),
+    password: PG_PASSWORD,
+    ssl: NODE_ENV === "production" ? { rejectUnauthorized: false } : undefined,
   });
+  return pg;
 };
