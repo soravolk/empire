5b104ad3f815c8f2967eb49b1fe9ac07072606d0
soravolk <volk.huang@gmail.com>
2026-01-19 21:36:23 +0100

    Use repeat icon for routine type of milestone

--- web/src/components/MilestoneView.tsx
+++ web/src/components/MilestoneView.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { MdDelete, MdEdit } from "react-icons/md";
+import { BsArrowRepeat } from "react-icons/bs";
 import {
   useCreateMilestoneMutation,
   useDeleteMilestoneMutation,
@@ -530,14 +531,18 @@ export default function MilestoneView({
                         }`}
                         title="Double-click to view tasks"
                       >
-                        <svg
-                          className="w-8 h-8 text-white"
-                          fill="currentColor"
-                          stroke="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                        </svg>
+                        {milestone.type === "routine" ? (
+                          <BsArrowRepeat className="w-8 h-8 text-white" />
+                        ) : (
+                          <svg
+                            className="w-8 h-8 text-white"
+                            fill="currentColor"
+                            stroke="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
+                          </svg>
+                        )}
                       </button>
                     ))}
 
