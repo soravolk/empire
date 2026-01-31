64e4b7e10159e8240a220e606be19c5c28f57ba7
denpo1022 <mario159357@gmail.com>
2026-01-31 23:08:23 +0800

    Revert "ref EPR-176: Add backend tests for Category rename API"
    This reverts commit e5b117d3b3739bf5de7dc19a09ceda68168fad5b.

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  testMatch: ["**/__tests__/**/*.test.ts"],
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
-  coverageReporters: ["text", "text-summary"],
-};
